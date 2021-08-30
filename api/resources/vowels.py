500a4d336d1a91c73eb5208370b5e716fe991e7e
Timothy Kamau <tim@Timothys-MacBook-Pro.local>
2021-08-30 15:29:29 +0300

    code cleanup

--- api/resources/vowels.py
+++ api/resources/vowels.py
@@ -1,16 +1,12 @@
 from api.common.reverse_vowels import reverse_vowels
-from flask import request
 from flask_restful import Resource, reqparse
 
 parser = reqparse.RequestParser()
-parser.add_argument('message', required=True, help="Message cannot be blank!")
+parser.add_argument('message', required=True, help="Message is a required field!")
 
 
 class ReverseVowel(Resource):
     def post(self):
-        if request.method != 'POST':
-            return "The method is not allowed for the requested URL.", 405
-
         args = parser.parse_args()
         message = args['message']
 
