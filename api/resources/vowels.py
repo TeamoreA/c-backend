3f4bc261aad3fe0d486a725cb6f33155af5bc757
Timothy Kamau <tim@Timothys-MacBook-Pro.local>
2021-08-30 15:23:40 +0300

    add functionality to return 405 error

--- api/resources/vowels.py
+++ api/resources/vowels.py
@@ -1,5 +1,6 @@
-from flask_restful import reqparse, Resource
 from api.common.reverse_vowels import reverse_vowels
+from flask import request
+from flask_restful import Resource, reqparse
 
 parser = reqparse.RequestParser()
 parser.add_argument('message', required=True, help="Message cannot be blank!")
@@ -7,6 +8,9 @@ parser.add_argument('message', required=True, help="Message cannot be blank!")
 
 class ReverseVowel(Resource):
     def post(self):
+        if request.method != 'POST':
+            return "The method is not allowed for the requested URL.", 405
+
         args = parser.parse_args()
         message = args['message']
 
